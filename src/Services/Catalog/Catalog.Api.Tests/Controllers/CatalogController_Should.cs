00e9dc32a3ac8d4c4b0d44ccd57d947040cea5d3
Shaun Sargent <shaunm10@gmail.com>
2021-12-10 15:15:59 -0500

    - working on Unit Tests.

--- src/Services/Catalog/Catalog.Api.Tests/Controllers/CatalogController_Should.cs
+++ src/Services/Catalog/Catalog.Api.Tests/Controllers/CatalogController_Should.cs
@@ -1,13 +1,13 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Catalog.Api.Controllers;
 using Catalog.Api.Entities;
 using Catalog.Api.Repositories;
 using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Moq;
+using RandomTestValues;
 using Xunit;
 
 namespace Catalog.Api.Tests.Controllers;
@@ -27,7 +27,7 @@ public class CatalogController_Should
     }
 
     [Fact]
-    public async Task GetProducts_From_GetProducts()
+    public async Task ReturnProducts_From_GetProducts()
     {
         // arrange:
         var products = new List<Product>();
@@ -37,9 +37,65 @@ public class CatalogController_Should
         var response = await this.controllerUnderTest.GetProducts();
 
         // assert:
-        response.Should().NotBeNull();
-        response.Result.Value.Should().BeSameAs(products);
+        var okResult = response?.Result as OkObjectResult;
+        okResult.Should().NotBeNull();
+        okResult?.Value.Should().BeSameAs(products);
     }
 
+    [Fact]
+    public async Task ReturnProduct_From_GetProductsById()
+    {
+          // arrange:
+        var productId = RandomValue.String();
+        var product = new Product();
+        this.productRepositoryMock.Setup(x => x.GetProduct(productId)).ReturnsAsync(product);
+
+        // act:
+        var response = await this.controllerUnderTest.GetProductsById(productId);
+
+        // assert:
+        var okResult = response?.Result as OkObjectResult;
+        okResult.Should().NotBeNull();
+        okResult?.Value.Should().BeSameAs(product);
+    }
+
+     [Fact]
+    public async Task ReturnNotFound_From_GetProductsById()
+    {
+          // arrange:
+        var productId = RandomValue.String();
+        var product = new Product();
+        this.productRepositoryMock
+            .Setup(x => x.GetProduct(productId))
+            .ReturnsAsync((Product)null);
+
+        // act:
+        var response = await this.controllerUnderTest.GetProductsById(productId);
+
+        // assert:
+        var notFoundResult = response?.Result as NotFoundResult;
+        notFoundResult.Should().NotBeNull();
+        //this.loggerStub.Verify(x => x.LogError($"Product with id: {productId}, not found."), times: Times.Once);
+        
+    }
+
+    [Fact]
+    public async Task ReturnProduct_From_GetProductsByCategory()
+    {
+        // arrange:
+        var category = RandomValue.String();
+        var products =  new List<Product>();
+        this.productRepositoryMock
+            .Setup(x => x.GetProductsByCategory(category))
+            .ReturnsAsync(products);
+
+        // act:
+        var response = await this.controllerUnderTest.GetProductsByCategory(category);
+  
+        // assert:
+        var okResult = response?.Result as OkObjectResult;
+        okResult.Should().NotBeNull();
+        okResult?.Value.Should().BeSameAs(products);
+    }
 
 }
--- src/Services/Catalog/Catalog.Api/Program.cs
+++ src/Services/Catalog/Catalog.Api/Program.cs
@@ -13,7 +13,6 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<ICatalogContext, CatalogContext>();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
