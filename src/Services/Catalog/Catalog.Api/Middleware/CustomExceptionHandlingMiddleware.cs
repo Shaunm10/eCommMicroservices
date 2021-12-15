5e4909fbb26bef4cf125f4beaa511696c33f2afc
Shaun Sargent <shaunm10@gmail.com>
2021-12-14 19:42:08 -0500

    Added Exception Handler

---
+++ src/Services/Catalog/Catalog.Api/Middleware/CustomExceptionHandlingMiddleware.cs
@@ -0,0 +1,50 @@
+﻿using System.Net;
+
+namespace Catalog.Api.Middleware
+{
+    public class CustomExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<CustomExceptionHandlingMiddleware> _logger;
+
+        public CustomExceptionHandlingMiddleware(RequestDelegate next, ILogger<CustomExceptionHandlingMiddleware> logger)
+        {
+            this._next = next;
+            this._logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context) 
+        {
+            try 
+            {
+                await _next(context);
+            }
+            catch (Exception ex) 
+            {
+                await HandleGlobalException(context, ex);
+            }
+        }
+
+        private Task HandleGlobalException(HttpContext context, Exception ex)
+        {
+            if (ex is ApplicationException)
+            {
+                this._logger.LogWarning($"Validaton error occured in Api. {ex.Message}");
+                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                return context.Response.WriteAsJsonAsync(new { ex.Message});
+            }
+            else 
+            {
+                var errorId = Guid.NewGuid();
+                this._logger.LogError(ex,$"Error occured in Api:{errorId}");
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                return context.Response.WriteAsJsonAsync(new 
+                {
+                    ErrorId=errorId,
+                    Message ="Something bad happend in our Api." +
+                    "Contact our support team with the ErrorId if the issue persists."
+                });
+            }
+        }
+    }
+}
--- src/Services/Catalog/Catalog.Api/Program.cs
+++ src/Services/Catalog/Catalog.Api/Program.cs
@@ -1,4 +1,5 @@
 using Catalog.Api.Data;
+using Catalog.Api.Middleware;
 using Catalog.Api.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,6 +15,8 @@ builder.Services.AddScoped<ICatalogContext, CatalogContext>();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
+
+app.UseMiddleware<CustomExceptionHandlingMiddleware>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
