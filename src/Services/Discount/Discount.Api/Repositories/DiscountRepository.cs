32d1a4787bc3db33cff492ed2941b0149a4a092d
Shaun Sargent <shaunm10@gmail.com>
2021-12-24 16:55:32 -0500

    - added Coupon Entites and Repo.

---
+++ src/Services/Discount/Discount.Api/Repositories/DiscountRepository.cs
@@ -0,0 +1,27 @@
+﻿using Discount.Api.Entities.V1;
+
+namespace Discount.Api.Repositories
+{
+    public class DiscountRepository : IDiscountRepository
+    {
+        public Task<bool> CreateDiscount(Coupon coupon)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteDiscount(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Coupon> GetDiscount(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateDiscount(Coupon coupon)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Services/Discount/Discount.Api/Repositories/IDiscountRepository.cs
@@ -0,0 +1,17 @@
+﻿using Discount.Api.Entities.V1;
+
+namespace Discount.Api.Repositories;
+
+public interface IDiscountRepository
+{
+    Task<Coupon> GetDiscount(int productId);
+
+    Task<bool> CreateDiscount(Coupon coupon);
+
+    Task<bool> UpdateDiscount(Coupon coupon);
+
+    Task<bool> DeleteDiscount(int productId);
+
+
+}
+
