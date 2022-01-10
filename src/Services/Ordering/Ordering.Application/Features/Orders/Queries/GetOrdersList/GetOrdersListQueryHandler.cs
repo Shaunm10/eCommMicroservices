bf62f7515bc30ddc32898a347d3a937b06c5c134
Shaun Sargent <ShaunM10@gmail.com>
2022-01-10 13:54:49 -0500

    - adding constructor to the GetOrdersListQueryHandler.

--- src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListQueryHandler.cs
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListQueryHandler.cs
@@ -1,10 +1,21 @@
+using AutoMapper;
 using MediatR;
+using Ordering.Application.Contracts.Persistence;
 using Ordering.Application.Models;
 
 namespace Ordering.Application.Features.Orders.Queries.GetOrdersList;
 
 public class GetOrdersListQueryHandler : IRequestHandler<GetOrdersListQuery, List<OrdersVM>>
 {
+    private readonly IOrderRepository _orderRepository;
+    private readonly IMapper _mapper;
+
+    public GetOrdersListQueryHandler(IOrderRepository orderRepository, IMapper mapper)
+    {
+        this._mapper = mapper;
+        this._orderRepository = orderRepository;
+
+    }
     public Task<List<OrdersVM>> Handle(GetOrdersListQuery request, CancellationToken cancellationToken)
     {
         throw new NotImplementedException();
