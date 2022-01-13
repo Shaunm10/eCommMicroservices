97733546dfbcc883a84f99db9b0461bc1f486cc7
Shaun Sargent <ShaunM10@gmail.com>
2022-01-12 22:39:46 -0500

    - working on OrderCommandHandler.

--- src/Services/Ordering/Ordering.Application/Features/Orders/Commands/UpdateOrder/UpdateOrderCommand.cs
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/UpdateOrder/UpdateOrderCommand.cs
@@ -1,5 +1,35 @@
+using MediatR;
+
 namespace Ordering.Application.Features.Orders.Commands.UpdateOrder;
 
-public class UpdateOrderCommand
+public class UpdateOrderCommand : IRequest
 {
+
+    public string? UserName { get; set; }
+
+    public decimal TotalPrice { get; set; }
+
+    // BillingAddress
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set; }
+
+    public string? EmailAddress { get; set; }
+
+    public string? AddressLine { get; set; }
+
+    public string? Country { get; set; }
+
+    public string? State { get; set; }
+
+    public string? ZipCode { get; set; }
+
+    // Payment
+    public string? CardName { get; set; }
+
+    public string? CardNumber { get; set; }
+
+    public string? Expiration { get; set; }
+
+    public string? CVV { get; set; }
 }
--- src/Services/Ordering/Ordering.Application/Features/Orders/Commands/UpdateOrder/UpdateOrderCommandHandler.cs
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/UpdateOrder/UpdateOrderCommandHandler.cs
@@ -1,5 +1,29 @@
+using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Contracts.Infrastructure;
+using Ordering.Application.Contracts.Persistence;
+
 namespace Ordering.Application.Features.Orders.Commands.UpdateOrder;
 
-public class UpdateOrderCommandHandler
+public class UpdateOrderCommandHandler : IRequestHandler<UpdateOrderCommand>
 {
+    private readonly IOrderRepository _orderRepository;
+    private readonly IMapper _mapper;
+    private readonly ILogger<UpdateOrderCommandHandler> _logger;
+
+    public UpdateOrderCommandHandler(IOrderRepository orderRepository, IMapper mapper, ILogger<UpdateOrderCommandHandler> logger)
+    {
+        this._logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        this._mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        this._orderRepository = orderRepository ?? throw new ArgumentNullException(nameof(orderRepository));
+
+    }
+
+    public async Task<Unit> Handle(UpdateOrderCommand request, CancellationToken cancellationToken)
+    {
+        this.
+
+        return new Unit();
+    }
 }
