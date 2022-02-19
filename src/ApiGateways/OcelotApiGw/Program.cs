072480b67214d99e40bbfa80899ef8dc3b066a58
“Shaun <shaunm10@gmail.com>
2022-02-19 16:15:44 -0500

    - Added ocelot package to project. - Added ocelot.json + ocelot.Developement.json

--- src/ApiGateways/OcelotApiGw/Program.cs
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -1,5 +1,18 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// the new way to do this in .net 6
+builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
+
+builder.Services.AddOcelot();
+
 var app = builder.Build();
+
+await app.UseOcelot();
 
 app.MapGet("/", () => "Hello World!");
 
