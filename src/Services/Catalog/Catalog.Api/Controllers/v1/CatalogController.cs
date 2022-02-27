a928345670dcb3913a05eb2c74c1a9f8c22aaa02
Shaun Sargent <shaunm10@gmail.com>
2022-02-27 00:00:36 -0500

    - tested Ocelot GW with microserives.

--- src/Services/Catalog/Catalog.Api/Controllers/v1/CatalogController.cs
+++ src/Services/Catalog/Catalog.Api/Controllers/v1/CatalogController.cs
@@ -26,7 +26,7 @@ namespace Catalog.Api.Controllers.v1
             return this.Ok(products);
         }
 
-        [HttpGet("[action]/{id:length(24)}", Name = "GetProductsById")]
+        [HttpGet("{id:length(24)}")]
         [ProducesResponseType((int)HttpStatusCode.NotFound)]
         [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
         public async Task<ActionResult<Product>> GetProductsById(string id)
