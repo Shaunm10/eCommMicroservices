1324c9584640206771604b49dc077cda9be5fc4f
“Shaun <shaunm10@gmail.com>
2022-03-13 11:00:52 -0400

    Added Models for Shopping.Aggregator

---
+++ src/ApiGateways/Shopping.Aggregator/Models/BasketModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Shopping.Aggregator.Models;
+
+public class BasketModel
+{
+    public string? UserName { get; set; }
+
+    public List<BasketItemExtendedModel> Items { get; set; } = new List<BasketItemExtendedModel>();
+
+    public decimal? TotalPrice { get; set; }
+
+    public string? Category { get; set; }
+
+    public string? Summary { get; set; }
+
+    public string? Description { get; set; }
+
+    public string? ImageFile { get; set; }
+}
