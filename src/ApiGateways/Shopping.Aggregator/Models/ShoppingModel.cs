c0c285c343554c04053a20ecd6e231653e7e3c97
“Shaun <shaunm10@gmail.com>
2022-03-13 11:32:19 -0400

    - fixing warnings. - adding ICatalogService.

--- src/ApiGateways/Shopping.Aggregator/Models/ShoppingModel.cs
+++ src/ApiGateways/Shopping.Aggregator/Models/ShoppingModel.cs
@@ -3,7 +3,9 @@ namespace Shopping.Aggregator.Models
     public class ShoppingModel
     {
         public string? UserName { get; set; }
-        public BasketModel BasketWithProducts { get; set; }
-        public IEnumerable<OrderResponseModel> Orders { get; set; }
+
+        public BasketModel? BasketWithProducts { get; set; }
+
+        public IEnumerable<OrderResponseModel> Orders { get; set; } = new List<OrderResponseModel>();
     }
 }
