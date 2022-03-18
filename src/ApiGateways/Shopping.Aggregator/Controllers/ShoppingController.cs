e845f478753eac8c91c2d79740b041fc1969eeb0
Shaun Sargent <ssargent@costar.com>
2022-03-18 16:16:53 -0400

    Added ShoppingController

---
+++ src/ApiGateways/Shopping.Aggregator/Controllers/ShoppingController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shopping.Aggregator.Services;
+
+namespace Shopping.Aggregator.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class ShoppingController : ControllerBase
+{
+    private readonly ICatalogService catalogService;
+    private readonly IBasketService basketService;
+    private readonly IOrderService orderService;
+
+    public ShoppingController(ICatalogService catalogService, IBasketService basketService, IOrderService orderService)
+    {
+        this.catalogService = catalogService ?? throw new ArgumentNullException(nameof(catalogService));
+        this.basketService = basketService ?? throw new ArgumentNullException(nameof(basketService));
+        this.orderService = orderService ?? throw new ArgumentNullException(nameof(orderService));
+    }
+}
