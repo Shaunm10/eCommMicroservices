54379c9b13e091632e74ce9102dc495a1c53521e
Shaun Sargent <shaunm10@gmail.com>
2022-05-10 16:04:32 -0400

    - working on Cart Refactoring.

--- src/WebApps/AspnetRunBasics/Pages/Cart.cshtml.cs
+++ src/WebApps/AspnetRunBasics/Pages/Cart.cshtml.cs
@@ -1,33 +1,50 @@
-﻿using System;
-using System.Threading.Tasks;
-using AspnetRunBasics.Entities;
-using AspnetRunBasics.Repositories;
+﻿using AspnetRunBasics.Models;
+using AspnetRunBasics.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace AspnetRunBasics
 {
     public class CartModel : PageModel
     {
-        private readonly ICartRepository _cartRepository;
+        //private readonly ICartRepository _cartRepository;
+        private readonly IBasketService _basketService;
 
-        public CartModel(ICartRepository cartRepository)
+        public CartModel(IBasketService basketService)
         {
-            _cartRepository = cartRepository ?? throw new ArgumentNullException(nameof(cartRepository));
+            this._basketService = basketService ?? throw new ArgumentNullException(nameof(basketService));
+            //_cartRepository = cartRepository ?? throw new ArgumentNullException(nameof(cartRepository));
         }
 
-        public Entities.Cart Cart { get; set; } = new Entities.Cart();        
+        public BasketModel Cart { get; set; } = new BasketModel();
 
         public async Task<IActionResult> OnGetAsync()
         {
-            Cart = await _cartRepository.GetCartByUserName("test");            
+            var userName = "jts";
+            Cart = await this._basketService.GetBasketAsync(userName);
 
             return Page();
         }
 
-        public async Task<IActionResult> OnPostRemoveToCartAsync(int cartId, int cartItemId)
+        public async Task<IActionResult> OnPostRemoveToCartAsync(string productId)
         {
-            await _cartRepository.RemoveItem(cartId, cartItemId);
+            var userName = "jts";
+            var basket = await this._basketService.GetBasketAsync(userName);
+
+            // get the basket
+            var item = basket.Items.FirstOrDefault(x => x.ProductId == productId);
+
+            if (item != null)
+            {
+                basket.Items.Remove(item);
+            }
+
+            // update the DB
+            var basketUpdated = await this._basketService.UpdateBasketAsync(basket);
+
             return RedirectToPage();
         }
     }
