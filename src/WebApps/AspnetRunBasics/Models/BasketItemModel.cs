a6bb298b24b4469ab74f3747c5e849b859c8a6dc
Shaun Sargent <shaunm10@gmail.com>
2022-05-10 09:48:29 -0400

    Added new Models for webApp

---
+++ src/WebApps/AspnetRunBasics/Models/BasketItemModel.cs
@@ -0,0 +1,10 @@
+﻿namespace AspnetRunBasics.Models;
+
+public class BasketItemModel
+{
+    public int Quantity { get; set; } = 1;
+    public string? Color { get; set; }
+    public decimal Price { get; set; } = 0;
+    public string? ProductId { get; set; }
+    public string? ProductName { get; set; }
+}
